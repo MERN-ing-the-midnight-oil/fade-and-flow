286bbed97793d612c8270adf4e67302db2b98323
Rhys Smoker <r.smoker@gmail.com>
2023-09-14 17:15:11 -0700

    updated text

--- src/App.js
+++ src/App.js
@@ -1,152 +1,103 @@
 import React, { useState, useEffect } from "react";
 import { useTransition, animated } from "react-spring";
 
-const imagesPour = [
-	`${process.env.PUBLIC_URL}/pour/pour1.png`,
-	`${process.env.PUBLIC_URL}/pour/pour2.png`,
-	`${process.env.PUBLIC_URL}/pour/pour3.png`,
-];
-const imagesQwerty = [
-	`${process.env.PUBLIC_URL}/qwerty/qwerty1.png`,
-	`${process.env.PUBLIC_URL}/qwerty/qwerty2.png`,
-	`${process.env.PUBLIC_URL}/qwerty/qwerty3.png`,
-];
-const imagesStein = [
-	`${process.env.PUBLIC_URL}/stein/stein1.png`,
-	`${process.env.PUBLIC_URL}/stein/stein2.png`,
-	`${process.env.PUBLIC_URL}/stein/stein3.png`,
-	`${process.env.PUBLIC_URL}/stein/stein4.png`,
-	`${process.env.PUBLIC_URL}/stein/stein5.png`,
-	`${process.env.PUBLIC_URL}/stein/stein6.png`,
-];
+const imageGroups = {
+	pour: [
+		`${process.env.PUBLIC_URL}/pour/pour1.png`,
+		`${process.env.PUBLIC_URL}/pour/pour2.png`,
+		`${process.env.PUBLIC_URL}/pour/pour3.png`,
+	],
+	qwerty: [
+		`${process.env.PUBLIC_URL}/qwerty/qwerty1.png`,
+		`${process.env.PUBLIC_URL}/qwerty/qwerty2.png`,
+		`${process.env.PUBLIC_URL}/qwerty/qwerty3.png`,
+	],
+	stein: [
+		`${process.env.PUBLIC_URL}/stein/stein1.png`,
+		`${process.env.PUBLIC_URL}/stein/stein2.png`,
+		`${process.env.PUBLIC_URL}/stein/stein3.png`,
+		`${process.env.PUBLIC_URL}/stein/stein4.png`,
+	],
+};
 
 const fadeDuration = 2000;
 
 function PreloadImages() {
 	return (
 		<div style={{ display: "none" }}>
-			{[...imagesPour, ...imagesQwerty, ...imagesStein].map((src, index) => (
-				<img
-					key={index}
-					src={src}
-					alt={`preload-image-${index}`}
-				/>
-			))}
+			{Object.values(imageGroups)
+				.flat()
+				.map((src, index) => (
+					<img
+						key={index}
+						src={src}
+						alt={`preload-image-${index}`}
+					/>
+				))}
 		</div>
 	);
 }
 
-function App() {
-	const [currentImageIndexPour, setCurrentImageIndexPour] = useState(0);
-	const [currentImageIndexQwerty, setCurrentImageIndexQwerty] = useState(0);
-	const [currentImageIndexStein, setCurrentImageIndexStein] = useState(0);
+function AnimatedImage({ group, positionTop }) {
+	const [currentIndex, setCurrentIndex] = useState(0);
 
 	useEffect(() => {
-		const intervalPour = setInterval(() => {
-			setCurrentImageIndexPour(
-				(prevIndex) => (prevIndex + 1) % imagesPour.length
-			);
+		const interval = setInterval(() => {
+			setCurrentIndex((prev) => (prev + 1) % imageGroups[group].length);
 		}, fadeDuration * 2);
 
-		const intervalQwerty = setInterval(() => {
-			setCurrentImageIndexQwerty(
-				(prevIndex) => (prevIndex + 1) % imagesQwerty.length
-			);
-		}, fadeDuration * 2);
+		return () => clearInterval(interval);
+	}, [group]);
 
-		const intervalStein = setInterval(() => {
-			setCurrentImageIndexStein((prevIndex) => {
-				const newIndex = (prevIndex + 1) % imagesStein.length;
-				console.log("Updating Stein Image Index:", newIndex);
-				return newIndex;
-			});
-		}, fadeDuration * 2);
-
-		return () => {
-			clearInterval(intervalPour);
-			clearInterval(intervalQwerty);
-			clearInterval(intervalStein);
-		};
-	}, []);
-
-	const transitionsPour = useTransition(currentImageIndexPour, {
+	const transitions = useTransition(currentIndex, {
 		from: { opacity: 0, filter: "brightness(0.5)" },
 		enter: { opacity: 1, filter: "brightness(1)" },
 		leave: { opacity: 0, filter: "brightness(0.5)" },
-		config: {
-			duration: fadeDuration,
-		},
+		config: { duration: fadeDuration },
 	});
 
-	const transitionsQwerty = useTransition(currentImageIndexQwerty, {
-		from: { opacity: 0, filter: "brightness(0.5)" },
-		enter: { opacity: 1, filter: "brightness(1)" },
-		leave: { opacity: 0, filter: "brightness(0.5)" },
-		config: {
-			duration: fadeDuration,
-		},
-	});
+	return transitions((style, item) => (
+		<animated.img
+			src={imageGroups[group][item]}
+			alt={`${group}-image-${item}`}
+			style={{
+				...style,
+				position: "absolute",
+				top: positionTop,
+				left: 0,
+				right: 0,
+				bottom: 0,
+				zIndex: 0,
+			}}
+		/>
+	));
+}
 
-	const transitionsStein = useTransition(currentImageIndexStein, {
-		from: { opacity: 0, filter: "brightness(0.5)" },
-		enter: { opacity: 1, filter: "brightness(1)" },
-		leave: { opacity: 0, filter: "brightness(0.5)" },
-		config: {
-			duration: fadeDuration,
-		},
-	});
-
+function App() {
 	return (
 		<div className="App">
 			<PreloadImages />
 			<h1>Fade and Flow Animations</h1>
-			<header style={{ position: "relative", width: "100%", height: "400px" }}>
-				{transitionsPour((style, item) => (
-					<animated.img
-						src={imagesPour[item]}
-						alt={`pour-image-${item}`}
-						style={{
-							...style,
-							position: "absolute",
-							top: 0,
-							left: 0,
-							right: 0,
-							bottom: 0,
-							zIndex: 0,
-						}}
-					/>
-				))}
-				{transitionsQwerty((style, item) => (
-					<animated.img
-						src={imagesQwerty[item]}
-						alt={`qwerty-image-${item}`}
-						style={{
-							...style,
-							position: "absolute",
-							top: "400px",
-							left: 0,
-							right: 0,
-							bottom: 0,
-							zIndex: 0,
-						}}
-					/>
-				))}
-
-				{transitionsStein((style, item) => (
-					<animated.img
-						src={imagesStein[item]}
-						alt={`steins-image-${item}`}
-						style={{
-							...style,
-							position: "absolute",
-							top: "800px", // Adjust as needed
-							left: 0,
-							right: 0,
-							bottom: 0,
-							zIndex: 0,
-						}}
-					/>
-				))}
+			<h2>
+				I created the following animations as examples of what one can do by
+				upscaling and versioning an image repeatedly using Midjourney AI. Please
+				visit https://www.youtube.com/watch?v=1T798gp0cPI&ab_channel=RhysSmoker
+				to watch a three minute tutorial on how to generate a series of images
+				that will look great in this format.{" "}
+			</h2>
+			<header style={{ position: "relative", width: "100%", height: "1200px" }}>
+				<AnimatedImage
+					group="pour"
+					positionTop="0px"
+				/>
+				<AnimatedImage
+					group="qwerty"
+					positionTop="400px"
+				/>
+				<AnimatedImage
+					group="stein"
+					positionTop="800px"
+				/>
 			</header>
 		</div>
 	);
